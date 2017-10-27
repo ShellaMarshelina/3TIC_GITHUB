c9bb39c42fdd83ae679e332cd4c1e357646cf723
ShellaMarshelina <shella15ti@mahasiswa.pcr.ac.id>
2017-10-27 09:24:25 +0700

    Telah ditambah web form baru.

---
+++ Shella_github/Githubpertama/Tesweb.aspx.vb
@@ -0,0 +1,8 @@
+﻿Public Class Tesweb
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Label1.Text = "selamat datang"
+    End Sub
+
+End Class
